8da01ddfeb316263616902375f243ffc299ea8d7
Shrutigupta431 <sg9956855@gmail.com>
2026-05-10 23:09:14 +0530

    multiple-handler

--- src/app.js
+++ src/app.js
@@ -1,65 +1,22 @@
 const express = require("express");
-const app = express(); //creating new application/server using express framework
-
-//work for /ac & /abc
-app.get(/^\/ab?c$/, (req, res) => {
-    
-    res.send({
-        firstName: "John",
-        lastName: "Doe"
-    });
-});
-//work for /ac & /abc
-app.get(/^\/a(bc)?d$/, (req, res) => {
-    res.send({
-        firstName: "John",
-        lastName: "Doe"
-    });
-});
-//work for /bcd & /bcccccccccccccccd
-app.get(/^\/bc+d$/, (req, res) => {
-    res.send({
-        firstName: "John",
-        lastName: "Doe"
-    });
-});//work for /efgh & /effffffffffffffgh 
-app.get(/^\/ef*gh$/, (req, res) => {
-    res.send({
-        firstName: "John",
-        lastName: "Doe"
-    });
-});
-//work for /efgh & /efffffffffdhwjdihddodfffffgh
-app.get(/^\/ef.*gh$/, (req, res) => {
-    res.send({
-        firstName: "John",
-        lastName: "Doe"
-    });
-});
-//for query http://localhost:3000/user?id=101&password=Shruu@431
-app.get("/user",(req,res)=>{
-    console.log(req.query);
-    res.send({firstName:"John",lastName:"Doe"});
-});
-//for params : http://localhost:3000/user/101/shruu
-app.get("/user/:userId/:name",(req,res)=>{
-    console.log(req.params);
-    res.send({firstName:"John",lastName:"Doe"});
-});
-// app.post("/user",(req,res)=>{
-//     res.send("User created successfully!"); //request handler for POST request to /user endpoint
-// });
-// app.delete("/user",(req,res)=>{
-//     res.send("User deleted successfully!"); //request handler for DELETE request to /deleteUser endpoint
-// })
-// app.use("/hello",(req,res)=>{
-//     res.send("Hello from the /hello!"); //request handler for /hello endpoint
-// });//this handler will run only if app.use("/",(req,res)=>{ this handler is written below this handler . Order is very important 
+const app = express();
 
 
-// app.use("/",(req,res)=>{
-//     res.send("/Hllo!")
-// }); //request handler for all incoming requests, sending a response "Hello from the server!"
-app.listen(3000,()=>{
-    console.log("Server is running on port 3000");  
+app.use("/user",//route
+  [  (req, res,next) => {
+        // res.send("Route Handler 1:");//route handler 
+        next();
+    },
+    (req, res,next) => {
+        // res.send("Route Handler 2: ");//route handler 
+        next();
+    },
+     (req, res) => {
+        res.send("Route Handler 3: ");//route handler 
+    },]
+);
+
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
 });

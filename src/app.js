abda45fbb90838d74097f47424a4050d8080b354
Shrutigupta431 <sg9956855@gmail.com>
2026-05-25 15:10:50 +0530

    remove-spaces

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ require("dotenv").config();
 
 const cors = require("cors")
 app.use(cors({
-    origin :"http://localhost:5174      ",
+    origin :"http://localhost:5173",
     credentials:true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"]
 }));
